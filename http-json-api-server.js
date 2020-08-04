8b2fccd029a8bab7c312ffd185ad5160d0ecf1dd
Pete Jones <pwj2012@gmail.com>
2020-08-04 11:33:20 -0700

    got it

--- http-json-api-server.js
+++ http-json-api-server.js
@@ -1,31 +1,29 @@
 const http = require('http')
-const url = require('url') // same error if I comment this out
+const url = require('url')
 const port = process.argv[2]
 const server = http.createServer((req, res) => {
     req.setEncoding('utf-8')
     res.writeHead(200, {'Content-Type': 'application/json'})
-    // console.log('http://localhost:' + port +req.url)
-    // console.log(port)
-    const url = new URL('http://localhost:' + port + req.url) // error happens here
+    const url = new URL('http://localhost:' + port + req.url)
     const endpoint = url.pathname
     const iso = url.searchParams.get('iso')
     const date = new Date(iso)
-    console.log(url)
-    // console.log(endpoint)
-    console.log(iso)
-    console.log(date)
     if (endpoint === '/api/parsetime') {
-        console.log(endpoint)
+        console.log(JSON.stringify({
+            hour: date.getHours(),
+            minute: date.getMinutes(),
+            second: date.getSeconds()
+        }))
         res.write(JSON.stringify({
             hour: date.getHours(),
             minute: date.getMinutes(),
-            seconds: date.getSeconds()
+            second: date.getSeconds()
         }))
-        res.on
     } else if (endpoint === '/api/unixtime') {
         res.write(JSON.stringify({
             unixtime: date.getTime()
         }))
     }
+    res.end()
 })
 server.listen(port)
