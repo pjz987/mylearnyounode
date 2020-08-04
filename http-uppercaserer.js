d7ef65e45dd2f997a0313892925f68d772e32e5e
Pete Jones <pwj2012@gmail.com>
2020-08-04 11:16:13 -0700

    stuck on the last one

--- http-uppercaserer.js
+++ http-uppercaserer.js
@@ -1,5 +1,9 @@
 const http = require('http')
 const port = process.argv[2]
 const server = http.createServer((request, response) => {
+    request.setEncoding('utf-8')
+    request.on('data', (data) => {
+        response.write(data.toUpperCase())
+    })
 })
 server.listen(port)
